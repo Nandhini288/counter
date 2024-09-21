6ecc1758dad18878ed02c288ffa37f9cecfcf2e0
Nandhini <nandhusithan.28@gmail.com>
2024-09-21 09:37:20 +0530

    updated

---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import './counter.css'
+
+function Counter()
+{
+    var [count,setCount] = useState(0)
+
+    function increament()
+    {
+        var newCount = count+1
+        setCount(newCount)
+    }
+    function decreament()
+    {
+        var newCount = count-1
+        setCount(newCount)
+    }
+
+    return(
+        <div>
+            <h1>Counter</h1>
+            <table>
+                <tr>
+                    <td> <button onClick={increament}><span>+</span></button></td>
+                    <td><button onClick={decreament}><span>-</span></button></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><h1>{count}</h1></td>
+                </tr>
+            </table>
+        </div>
+    )
+    
+}
+export default Counter
